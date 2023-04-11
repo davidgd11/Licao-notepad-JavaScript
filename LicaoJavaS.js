078b2a530387fa561c3dd3be9883d89f3f11e16f
David Guilherme B. Denunci <daviddenunci@gmail.com>
2023-04-11 10:28:29 -0300

    Licao-JavaScript

---
+++ LicaoJavaS.js
@@ -0,0 +1,94 @@
+<script> 
+
+// 1) 
+let numFloata = 30.345
+console.log(parseInt(numFloata))
+
+let numFloatb = 154.666
+console.log(parseInt(numFloatb))
+
+let numFloatc = 50.888
+console.log(parseInt(numFloatc))
+
+let numFloatd = 089.52
+console.log(parseInt(numFloatd))
+
+
+
+//2)
+let Stringfloat1 = "30.345"
+console.log(parseFloat(Stringfloat1))
+
+let Stringfloat2 = "154.666"
+console.log(parseFloat(Stringfloat2))
+
+let Stringfloat3 = "50.888"
+console.log(parseFloat(Stringfloat3))
+
+let Stringfloat4 = "089.52"
+console.log(parseFloat(Stringfloat4))
+
+
+
+//3)
+let alta = "A MAIOR TECNOLOGIA"
+console.log(alta.length)
+
+let altb= "HARDWARE"
+console.log(altb.length)
+
+let altc= "FIAP A MELHOR FACULDADE"
+console.log(altc.length)
+
+let altd= "TECNOLOGIA"
+console.log(altd.length)
+
+
+
+//4)
+
+
+let texto2 = "HARDWARE FAZ O COMPUTADOR"
+console.log(texto2.indexOf("WARE"))
+
+let texto3 = "FIAP A MELHOR FACULDADE"
+console.log(texto3.indexOf("AP"))
+
+let texto4 = "TECNOLOGIA E INOVAÇÃO"
+console.log(texto4.indexOf("INO"))
+
+
+
+//5)
+let text = "A MAIOR TECNOLOGIA"
+console.log(text.lastIndexOf("TEC"))
+
+let text2 = "HARDWARE FAZ O COMPUTADOR"
+console.log(text2.lastIndexOf("DOR"))
+
+let text3 = "FIAP A MELHOR FACULDADE"
+console.log(text3.lastIndexOf("FACU"))
+
+let text4 = "TECNOLOGIA E INOVAÇÃO"
+console.log(text4.lastIndexOf("VAÇ"))
+
+
+
+//6)
+let loja = "LOJA DE "
+document.write(loja + "INFORMÁTICA")
+
+let preco = "R$22,00"
+document.write("O MELHOR MOUSE POR " + preco)
+
+let tec = "TRAS O MELHOR CONFORTO POR R$50,00"
+document.write("TECLADO QUE " + tec)
+
+let cadeira = "TOP DE LINHA R$1300,00"
+document.write("CADEIRA GAMER " + cadeira)
+
+let mouse = "MOUSE PAD POR R$ 15,00"
+document.write("UTILIZE O MELHOR " + mouse)
+
+
+</script>
